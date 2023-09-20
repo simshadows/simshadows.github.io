c6aad40d891620143648714c11a1ec774fb8746b
simshadows <contact@simshadows.com>
2023-09-20 14:49:20 +1000

    Changed URL to www.simshadows.com

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
-export const SITE = new URL("/", "https://pages.simshadows.com/").href;
+export const SITE = new URL("/", "https://www.simshadows.com/").href;
 export const SITE_TITLE = "simshadows";
 export const AUTHOR = "simshadows";
 export const COPYRIGHT_YEAR = 2023;
-export const SOURCE_CODE_URL = new URL("/", "https://github.com/simshadows/simshadows-pages").href;
+export const SOURCE_CODE_URL = new URL("/", "https://github.com/simshadows/simshadows.github.io").href;
 
 export const WEB_APPS_URL = new URL("/", "https://apps.simshadows.com/").href;
-export const BLOG_URL = new URL("/", "https://www.simshadows.com/").href;
+export const BLOG_URL = new URL("/", "https://blog.simshadows.com/").href;
 
