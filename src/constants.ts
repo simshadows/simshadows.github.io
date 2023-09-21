b8b1b5becf420ce9d8257aa974ff1b7fb0f944dd
simshadows <contact@simshadows.com>
2023-09-21 14:17:40 +1000

    Added global keywords

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,7 @@ export const SITE = new URL("/", "https://www.simshadows.com/").href;
 export const SITE_TITLE = "simshadows";
 export const AUTHOR = "simshadows";
 export const COPYRIGHT_YEAR = 2023;
+export const GLOBAL_KEYWORDS = ["simshadows"];
 export const SOURCE_CODE_URL = new URL("/", "https://github.com/simshadows/simshadows.github.io").href;
 
 export const WEB_APPS_URL = new URL("/", "https://apps.simshadows.com/").href;
