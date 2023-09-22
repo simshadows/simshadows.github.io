e19f454778b16ac40d2ca6cf7998c405fb5ac374
simshadows <contact@simshadows.com>
2023-09-23 01:12:22 +1000

    Added content to programming cheatsheets

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,10 @@
-export const SITE = new URL("/", "https://www.simshadows.com/").href;
+export const SITE = "https://www.simshadows.com/";
 export const SITE_TITLE = "simshadows";
 export const AUTHOR = "simshadows";
 export const COPYRIGHT_YEAR = 2023;
 export const GLOBAL_KEYWORDS = ["simshadows"];
-export const SOURCE_CODE_URL = new URL("/", "https://github.com/simshadows/simshadows.github.io").href;
+export const SOURCE_CODE_URL = "https://github.com/simshadows/simshadows.github.io";
 
-export const WEB_APPS_URL = new URL("/", "https://apps.simshadows.com/").href;
-export const BLOG_URL = new URL("/", "https://blog.simshadows.com/").href;
+export const WEB_APPS_URL = "https://apps.simshadows.com/";
+export const BLOG_URL = "https://blog.simshadows.com/";
 
