7e28e4e45f51ed8fdc07ecb86a5b14edcd6d642b
simshadows <contact@simshadows.com>
2023-10-23 14:31:32 +1100

    Migrated some notes on dynamic programming

--- src/latex-helpers/katex-config.ts
+++ src/latex-helpers/katex-config.ts
@@ -6,8 +6,18 @@ export const katexConfig = {
     macros: {
         "\\parens": "{\\left({#1}\\right)}",
         "\\brackets": "{\\left[{#1}\\right]}",
+        "\\braces": "{\\left\\{{#1}\\right\\}}",
         "\\floor": "{\\left\\lfloor{#1}\\right\\rfloor}",
         "\\Nth": "{{#1}^\\textit{#2}}",
+
+        // Hacky compatibility implementation of siunitx just so I can keep the same syntax.
+        // Ideally, KaTeX should properly support some subset of the siunitx API.
+        "\\qty": "{{#1}\\,{#2}}",
+        "\\kilo": "{\\text{k}}",
+        "\\gram": "{\\text{g}}",
+
+        // Some technical debt to figure out a better solution to
+        "\\dollars": "{\\${#1}}",
     },
 }
 
