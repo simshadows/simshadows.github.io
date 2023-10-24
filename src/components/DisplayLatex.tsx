622f1415d6030de3f273fc31b59c46f062fdf000
simshadows <contact@simshadows.com>
2023-10-24 22:28:44 +1100

    Fixed random unnecessary display equation scrollbars

--- src/components/DisplayLatex.tsx
+++ src/components/DisplayLatex.tsx
@@ -1,6 +1,8 @@
 import katex from "katex";
 import "katex/dist/katex.min.css";
 
+import "./DisplayLatex.css";
+
 import {getConfig} from "@root/latex-helpers/katex-config";
 
 interface Props {
@@ -11,7 +13,7 @@ export default function DisplayLatex({code}: Props) {
     const rawHTML = katex.renderToString(code, getConfig(true));
     return <p>
         <div
-            class="horizontally-scrolling-box"
+            class="display-latex horizontally-scrolling-box"
             dangerouslySetInnerHTML={{__html: rawHTML}}
         />
     </p>;
