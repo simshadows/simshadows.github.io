ba0471502aba8db7c55314bdf3200a26478d82c1
simshadows <contact@simshadows.com>
2023-12-28 12:46:11 +1100

    Added external links to homepage

--- src/constants.ts
+++ src/constants.ts
@@ -5,6 +5,9 @@ export const COPYRIGHT_YEAR = 2023;
 export const GLOBAL_KEYWORDS = ["simshadows"];
 export const SOURCE_CODE_URL = "https://github.com/simshadows/simshadows.github.io";
 
+export const PUBLIC_EMAIL = "contact@simshadows.com";
+
 export const WEB_APPS_URL = "https://apps.simshadows.com/";
 export const BLOG_URL = "https://blog.simshadows.com/";
+export const DISCORD_SERVER_URL = "https://discord.gg/xwV6UN4";
 
