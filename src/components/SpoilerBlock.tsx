30168f37c7fba0889a7932f0d64c3d474191c751
simshadows <contact@simshadows.com>
2024-01-02 00:34:28 +1100

    Implemented spoiler blocks

---
+++ src/components/SpoilerBlock.tsx
@@ -0,0 +1,47 @@
+import {Component} from "preact";
+
+import {type JSXChildren} from "@root/tech-debt";
+
+import "./SpoilerBlock.css";
+
+interface Props {
+    readonly children: JSXChildren;
+    readonly buttonLabel?: string;
+    readonly expandByDefault?: boolean;
+}
+
+interface State {
+    expanded: boolean;
+}
+
+export default class SpoilerBlock extends Component<Props, State> {
+    constructor(props: Props) {
+        super();
+        this.handleButtonClick = this.handleButtonClick.bind(this);
+
+        this.state = {
+            expanded: !!props.expandByDefault,
+        };
+    }
+
+    handleButtonClick() {
+        this.setState((state) => ({expanded: !state.expanded}));
+    }
+
+    render() {
+        const buttonLabel: string =
+            (this.props.buttonLabel)
+            ? this.props.buttonLabel
+            : "Spoiler";
+
+        return <p>
+            <div className="spoiler-block">
+                <button type="button" onClick={this.handleButtonClick}>
+                    {this.state.expanded ? `Hide ${buttonLabel}` : `Show ${buttonLabel}`}
+                </button>
+                {this.state.expanded && this.props.children}
+            </div>
+        </p>;
+    }
+}
+
