6906339bb4e938ac63dcbeb7dd68e93ba0925269
simshadows <contact@simshadows.com>
2024-01-25 09:25:00 +1100

    Completed a CSES problem

---
+++ src/pages/c/programming-challenges/cses/_solutions.sorting-and-searching/missing-coin-sum.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+input()
+coins = sorted(int(s) for s in input().strip().split())
+
+interval_len = 1 # INTERVAL STARTS AT AND INCLUDES ZERO
+for coin in coins:
+    if coin > interval_len:
+        break
+    interval_len += coin
+print(interval_len)
