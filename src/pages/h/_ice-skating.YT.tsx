4ba31f4663ff80f43f7220215e0b9cf3100c5cb6
simshadows <contact@simshadows.com>
2024-10-19 18:29:19 +1100

    Various changes

--- src/pages/h/_ice-skating.YT.tsx
+++ src/pages/h/_ice-skating.YT.tsx
@@ -3,14 +3,21 @@ interface Props {
     readonly title: string;
     readonly date:  string;
     readonly path:  string;
-    readonly timestamps?: string;
+    readonly start?: string;
+    readonly end?: string;
 }
 
 export default function YT(
-    {ch, title, date, path, timestamps=undefined}: Props
+    {ch, title, date, path, start=undefined, end=undefined}: Props
 ) {
     const href = `https://youtu.be/${path}`;
-    if (timestamps) title = `${title} (${timestamps})`;
-    return <>{ch}: <a href={href}><b>{title}</b></a> <em>({date})</em></>;
+    if (start && end) {
+        title = `${title} (${start} to ${end})`;
+    } else if (start) {
+        title = `${title} (${start})`;
+    } else if (end) {
+        throw new Error("End-only is not valid.");
+    }
+    return <>{ch}: <a href={href} target="_blank"><b>{title}</b></a> <em>({date})</em></>;
 }
 
