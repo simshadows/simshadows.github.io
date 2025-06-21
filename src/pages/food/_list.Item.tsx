49a7c66d35774e5dac39aea76bbabb98195ae55d
simshadows <contact@simshadows.com>
2025-06-22 01:25:53 +1000

    Added food delivery items to food list

--- src/pages/food/_list.Item.tsx
+++ src/pages/food/_list.Item.tsx
@@ -1,3 +1,12 @@
+/*
+ * Filename: _list.Item.tsx
+ * Author:   simshadows <contact@simshadows.com>
+ *
+ * Formats a single food item.
+ * By default, all food items are considered to be eat-in unless otherwise
+ * indicated via. the props.
+ */
+
 import {readDateStr} from "@helpers/props";
 
 const categoryRemaps: ReadonlyMap<string | undefined, string> = new Map([
@@ -9,7 +18,17 @@ interface Props {
     readonly d:  string; // Date, accepts ISO date, or "unknown"
     readonly n:  string; // Name
     readonly help?: string; // Helper Text
+
+    // Mutually exclusive flags
+    // (It is implied to be takeout if it is food delivery.)
     readonly isTakeout?: boolean;
+    readonly isDelivery?: boolean;
+
+    // Use this to record the cost of the item.
+    // It's optional free-form text.
+    // It is currently basically an unrendered comment, but the data
+    // will remain present in the markup just in case it's useful.
+    readonly cost?: string;
 }
 
 export default function Item(
@@ -18,7 +37,9 @@ export default function Item(
         d,
         n,
         help=undefined,
-        isTakeout=false
+        isTakeout=false,
+        isDelivery=false,
+        cost=undefined,
     }: Props
 ) {
     const categoryElem = (()=>{
@@ -37,6 +58,8 @@ export default function Item(
     })();
 
     isTakeout; // TODO: We should use this
+    isDelivery; // TODO: We should use this
+    cost; // Currently unused
     return <span>{categoryElem}{date}{n}{_help}</span>;
 }
 
